8e786849174ae515897109d77b9d8844c0086d8a
sphinx <mattis@sevenfelicce.de>
2018-06-16 20:57:39 +0200

    Added the first satellite, others can be activated

--- sat_recorder/include/m_station.h
+++ sat_recorder/include/m_station.h
@@ -8,15 +8,17 @@
 class m_station
 {
     public:
-        m_station();
+        m_station(CoordGeodetic geo, double minimum_elevation=10);
         virtual ~m_station();
 
-        m_satellite get_next_sat();
-
-        void setup();
+        m_satellite* get_next_sat();
 
     private:
         std::vector<m_satellite*> sat_list;
+        CoordGeodetic geo;
+        double minimum_elevation;
+
+        void setup();
 };
 
 #endif // M_STATION_H
