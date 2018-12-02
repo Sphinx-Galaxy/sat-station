814162fc5383f064a2bce0ce5809eb6c175a2b98
sphinx <mattis@sevenfelicce.de>
2018-12-02 14:21:02 +0100

    Change filename from SAT_DATE to DATE_SAT

--- main.cpp
+++ main.cpp
@@ -47,9 +47,9 @@ int main(int argc, char * argv[])
         sat = station.get_next_sat();
         std::cout << "Setting next sat:\nAOS: " << sat->get_aos_seconds() << " LOS: " << sat->get_los_seconds() << std::endl;
 
-        //time(&local_time);
+        time(&local_time);
         std::cout << "Waiting now: " << (sat->get_aos_seconds()-local_time) << " seconds" << std::endl;
-        sleep(sat->get_aos_seconds() - local_time);
+        //sleep(sat->get_aos_seconds() - local_time);
 
         //System call to kill all sdr
         //system("ps -ef | grep 'rtl_sdr' | grep -v grep | awk '{print $2}' | xargs -r kill -9");
